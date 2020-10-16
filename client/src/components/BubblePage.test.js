79dc9f8adfc9bbf3e96d6f043bd9e586f17d4e89
John Youn <john@Johns-MacBook-Pro.local>
2020-10-16 10:46:43 -0700

    edit delete works

--- client/src/components/BubblePage.test.js
+++ client/src/components/BubblePage.test.js
@@ -1,7 +1,36 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import BubblePage from "./BubblePage";
+import axiosGet from '../utils/axiosGet'
+jest.mock('../utils/axiosGet.js')
 
+const mockData = [
+  {
+    color: "aliceblue",
+    code: {
+      hex: "#f0f8ff"
+    },
+    id: 1
+  },
+  {
+    color: "limegreen",
+    code: {
+      hex: "#99ddbc"
+    },
+    id: 2
+  },
+  {
+    color: "aqua",
+    code: {
+      hex: "#00ffff"
+    },
+    id: 3
+  },
+]
+
+axiosGet.mockResolvedValue(mockData)
 test("Fetches data and renders the bubbles", () => {
   // Finish this test
+  render(<BubblePage/>)
+
 });
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { axiosWithAuth } from '../utils/axiosWithAuth'
+
+const initialFormValues = {
+  username: "",
+  password: ""
+}
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [formValues, setFormValues] = useState(initialFormValues)
+  const history = useHistory()
+
+  const changeHandler = e => {
+    setFormValues({...formValues, [e.target.name]: e.target.value})
+  }
+
+  const submitHandler = e => {
+    e.preventDefault()
+    axiosWithAuth()
+    .post("/api/login", formValues)
+    .then(res => {
+      console.log(res)
+      localStorage.setItem('token', res.data.payload)
+      history.push('/protected')
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form onSubmit={submitHandler}>
+        
+          <input
+           type="text" 
+          name="username" 
+          value={formValues.username}
+           onChange={changeHandler}
+           placeholder="username" >
+          </input>
+
+
+          <input
+           type="password" 
+          name="password" 
+          value={formValues.password}
+           onChange={changeHandler}
+           placeholder="password" >
+          </input>
+
+          <button>Log in</button>
+        
+      </form>
     </>
   );
 };
